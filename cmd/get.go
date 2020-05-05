0fd3e41bbfa85bb9985575bd07471bb60151639e
Pierre Mavro <deimos@deimos.fr>
2020-05-06 01:13:03 +0200

    feat: first release

---
+++ cmd/get.go
@@ -0,0 +1,156 @@
+package cmd
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/spf13/cobra"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Get Jeedom global summary",
+	Run: func(cmd *cobra.Command, args []string) {
+		result := make(map[string]string)
+
+		if res, _ := cmd.Flags().GetBool("sample"); res {
+			result = getSampleJeedomGlobalStatus()
+		} else {
+			apiKey, _ := cmd.Flags().GetString("apiKey")
+			url, _ := cmd.Flags().GetString("url")
+			result = getJeedomGlobalStatus(apiKey, url)
+		}
+
+		debugMode, _ := cmd.Flags().GetBool("debug")
+		prettyPrint(result, debugMode)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+	getCmd.Flags().StringP("url", "u", "", "Jeedom API URL")
+	getCmd.Flags().StringP("apiKey", "a", "", "Jeedom API key")
+	getCmd.Flags().BoolP("sample", "s", false,"Run a sample test")
+	getCmd.Flags().BoolP("debug", "d", false,"Run in debug mode")
+	getCmd.MarkFlagRequired("url")
+	getCmd.MarkFlagRequired("apiKey")
+}
+
+func getJeedomGlobalStatus(apiKey string, url string) map[string]string {
+	stringMap := make(map[string]string)
+	payload := map[string]interface{}{
+		"jsonrpc": "2.0",
+		"id":      "1",
+		"method":  "summary::global",
+		"params": map[string]string{
+			"apikey": apiKey,
+			"id":     "1",
+		},
+	}
+
+	bytesRepresentation, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(bytesRepresentation))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if resp.StatusCode != 200 {
+		fmt.Printf("Error while trying to reach url %s: %s", url, resp.Status)
+		os.Exit(1)
+	}
+
+	var result map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for key, value := range result {
+		if key == "error" {
+			for message, content := range value.(map[string]interface{}) {
+				if message == "message" {
+					fmt.Printf("Error: %s", content)
+					os.Exit(1)
+				}
+			}
+		} else if key == "result" {
+			for name, number := range result["result"].(map[string]interface{}) {
+				stringMap[name] = fmt.Sprintf("%v", number)
+			}
+		}
+	}
+
+	return stringMap
+}
+
+func getSampleJeedomGlobalStatus() map[string]string {
+	return map[string]string{
+		"alarm": "1", // ok
+		"door": "0",
+		"humidity": "1", // ok
+		"light": "1", // ok
+		"luminosity": "4", // ok
+		"motion": "0",
+		"outlet": "2", // ok
+		"power": "2", // ok
+		"security": "0",
+		"shutter": "0",
+		"temperature": "1", // ok
+		"windows": "3", // ok
+	}
+}
+
+func prettyPrint(jeedomMap map[string]string, debugMode bool) {
+	// icons: https://www.nerdfonts.com/cheat-sheet
+	var toPrint []string
+
+	if debugMode {
+		fmt.Println(jeedomMap)
+	}
+
+	for key, value := range jeedomMap {
+		if key == "alarm" && value != "0" {
+			toPrint = append(toPrint, "\uF023")
+			continue
+		} else if value == "<nil>" || value == "0" {
+			continue
+		} else if key == "security" {
+			toPrint = append(toPrint, value + "\uFC8D")
+		} else if key == "motion" {
+			toPrint = append(toPrint, value + "\uFC0C")
+		} else if key == "windows" {
+			toPrint = append(toPrint, value + "\uF17A")
+		} else if key == "outlet" {
+			toPrint = append(toPrint, value + "\uF1E6")
+		} else if key == "humidity" {
+			toPrint = append(toPrint, value + "\uE373")
+		} else if key == "light" {
+			toPrint = append(toPrint, value + "\uf834")
+		} else if key == "luminosity" {
+			toPrint = append(toPrint, value + "\ufAA7")
+		} else if key == "power" {
+			toPrint = append(toPrint, value + "\uF0E7")
+		} else if key == "door" {
+			toPrint = append(toPrint, value + "\uFD18")
+		} else if key == "temperature" {
+			toPrint = append(toPrint, value + "\uF2C7")
+		} else {
+			toPrint = append(toPrint, value + key)
+		}
+	}
+
+	lineToPrint := strings.Join(toPrint, " ")
+
+	fmt.Println(lineToPrint)
+	fmt.Println(lineToPrint)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/deimosfr/jeedom-status/cmd"
+
+func main() {
+	cmd.Execute()
+}
