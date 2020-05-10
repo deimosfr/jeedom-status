f2a2006617d89340e5a1036d5981476b6a49a3e1
Pierre Mavro <deimos@deimos.fr>
2020-05-10 18:09:47 +0200

    feat: adding bitbar script comments

--- integration/bitbar/jeedom-status.1m.sh
+++ integration/bitbar/jeedom-status.1m.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env sh
+#
+# Jeedom status, using jeedom-status (https://github.com/deimosfr/jeedom-status)
+#
+# <bitbar.title>Jeedom status</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Pierre Mavro (deimosfr)</bitbar.author>
+# <bitbar.author.github>deimosfr</bitbar.author.github>
+# <bitbar.desc>Jeedom global status for operating systems status bars</bitbar.desc>
+# <bitbar.dependencies>jeedom-status</bitbar.dependencies>
+# <bitbar.image>https://github.com/deimosfr/jeedom-status/blob/master/assets/mac_output.png?raw=true</bitbar.image>
+#
+# Dependencies:
+#   Jeedom-status (https://github.com/deimosfr/jeedom-status)
+
 APIKEY=""
 JEEDOM_URL=""
 /usr/local/bin/jeedom-status get --apiKey $APIKEY --url $JEEDOM_URL -s emoji
